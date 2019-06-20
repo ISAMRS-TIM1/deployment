961f7bdfe8cd30aab8d61c93d1dcc9d123be8274
Aleksandar Nedaković <aleksaleks123@gmail.com>
2019-06-20 18:54:23 +0200

    Removed .gitignore from model

---
+++ src/main/java/isamrs/tim1/model/HotelRoom.java
@@ -0,0 +1,157 @@
+package isamrs.tim1.model;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import isamrs.tim1.dto.HotelRoomDTO;
+
+@Entity
+@Table(name = "HotelRooms")
+public class HotelRoom implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "hotelroom_id", unique = true, nullable = false)
+	private Integer id;
+
+	@Column(name = "averageGrade", unique = false, nullable = false)
+	private Double averageGrade;
+
+	@Column(name = "numberOfPeople", unique = false, nullable = false)
+	private Integer numberOfPeople;
+
+	@Column(name = "roomNumber", unique = false, nullable = false)
+	private String roomNumber;
+
+	@Column(name = "defaultPriceOneNight", unique = false, nullable = false)
+	private Double defaultPriceOneNight;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "hotel")
+	private Hotel hotel;
+
+	@OneToMany(mappedBy = "hotelRoom", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private Set<SeasonalHotelRoomPrice> seasonalPrices;
+
+	@OneToMany(mappedBy = "hotelRoom", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private Set<HotelReservation> reservations;
+
+	@Column(name = "deleted", unique = false, nullable = false)
+	private boolean deleted = false;
+
+	public HotelRoom() {
+		super();
+		reservations = new HashSet<HotelReservation>();
+		seasonalPrices = new HashSet<SeasonalHotelRoomPrice>();
+	}
+
+	public HotelRoom(HotelRoomDTO hotelRoom, Hotel hotel) {
+		this.id = null;
+		this.averageGrade = 0.0;
+		this.numberOfPeople = hotelRoom.getNumberOfPeople();
+		this.defaultPriceOneNight = hotelRoom.getPrice();
+		this.roomNumber = hotelRoom.getRoomNumber();
+		this.hotel = hotel;
+		this.reservations = new HashSet<HotelReservation>();
+		this.seasonalPrices = new HashSet<SeasonalHotelRoomPrice>();
+		this.deleted = false;
+	}
+
+	public void update(HotelRoomDTO hotelRoom) {
+		this.numberOfPeople = hotelRoom.getNumberOfPeople();
+		this.defaultPriceOneNight = hotelRoom.getPrice();
+		this.roomNumber = hotelRoom.getRoomNumber();
+	}
+
+	public Double getAverageGrade() {
+		return averageGrade;
+	}
+
+	public void setAverageGrade(Double averageGrade) {
+		this.averageGrade = averageGrade;
+	}
+
+	public Integer getNumberOfPeople() {
+		return numberOfPeople;
+	}
+
+	public void setNumberOfPeople(Integer numberOfPeople) {
+		this.numberOfPeople = numberOfPeople;
+	}
+
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(String roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Double getDefaultPriceOneNight() {
+		return defaultPriceOneNight;
+	}
+
+	public void setDefaultPriceOneNight(Double defaultPriceOneNight) {
+		this.defaultPriceOneNight = defaultPriceOneNight;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Hotel getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+
+	public Set<HotelReservation> getReservations() {
+		return reservations;
+	}
+
+	public void setReservations(Set<HotelReservation> normalReservations) {
+		this.reservations = normalReservations;
+	}
+
+	public Set<SeasonalHotelRoomPrice> getSeasonalPrices() {
+		return seasonalPrices;
+	}
+
+	public void setSeasonalPrices(Set<SeasonalHotelRoomPrice> seasonalPrices) {
+		this.seasonalPrices = seasonalPrices;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	private static final long serialVersionUID = 1359942200118829407L;
+
+}
